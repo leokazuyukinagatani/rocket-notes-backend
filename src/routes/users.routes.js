06f313d72361163a574d12ebf8a7eefdc3ffcd8c
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-07-15 10:41:04 -0300

    config: routes for upload

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,4 +1,7 @@
 const { Router } = require("express");
+const multer = require('multer');
+const uploadConfig = require("../configs/upload");
+
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
@@ -8,10 +11,15 @@ const usersRoutes = Router();
 
 const usersController = new UsersController();
 
+const upload = multer(uploadConfig.MULTER);
 
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+  console.log(request.file.filename);
+  return response.json();
+});
 
 
 module.exports = usersRoutes;
