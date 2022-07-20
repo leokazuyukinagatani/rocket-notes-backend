21c5e8e6cb414aa6c6b4ab7da65e63a193eb6893
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2022-07-20 16:03:06 -0300

    tags

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -1,28 +1,21 @@
-const TagRepository = require("../repositories/TagRepository");
-const TagCreateService = require("../services/tag/TagCreateService");
-const TagIndexService = require("../services/tag/TagIndexService");
+const { response } = require("express");
+const knex = require("../database/knex");
 
 class TagsController {
 
   async index ( request, response ) {
     const user_id  = request.user.id;
 
-    const tagRepository = new TagRepository();
-    const tagIndexService = new TagIndexService(tagRepository);
+    const tags = await knex("tags")
+      .where({ user_id })
+      .groupBy("name");
 
-    const tags = tagIndexService.execute(user_id);
-    
     return response.json(tags);
   }
 
   async create( request, response )  { 
     const {note_id, name, user_id } = request.body;
-
-    const tagRepository = new TagRepository();
-    const tagCreateService = new TagCreateService(tagRepository);
-
-    await tagCreateService.execute({note_id, name, user_id});
-    return response.json();
+    await knex.insert(note_id, name, user_id);
   }
-  
+
 } module.exports = TagsController;
